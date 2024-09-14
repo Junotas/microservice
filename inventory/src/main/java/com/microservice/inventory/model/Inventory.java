68d4c4a1ef73185ff072f61427278438c3b6fe52
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-09-14 08:01:02 +0200

    created entity for inventory

--- inventory/src/main/java/com/microservice/inventory/model/Inventory.java
+++ inventory/src/main/java/com/microservice/inventory/model/Inventory.java
@@ -1,4 +1,60 @@
 package com.microservice.inventory.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.UuidGenerator;
+import java.util.UUID;
+
+@Entity
+@Table(name = "inventory")
 public class Inventory {
+
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    private String productName;
+    private int quantity;
+    private int reservedQuantity;
+
+    public Inventory() {}
+
+    public Inventory(String productName, int quantity, int reservedQuantity) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.reservedQuantity = reservedQuantity;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getReservedQuantity() {
+        return reservedQuantity;
+    }
+
+    public void setReservedQuantity(int reservedQuantity) {
+        this.reservedQuantity = reservedQuantity;
+    }
 }
