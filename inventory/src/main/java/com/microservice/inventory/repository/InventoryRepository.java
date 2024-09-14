e1b856a94f2e2e4a433e0f471f8b0f7ad3818c0f
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-09-14 08:17:52 +0200

    created JPA repository for inventory

--- inventory/src/main/java/com/microservice/inventory/repository/InventoryRepository.java
+++ inventory/src/main/java/com/microservice/inventory/repository/InventoryRepository.java
@@ -1,4 +1,12 @@
 package com.microservice.inventory.repository;
 
-public class InventoryRepository {
+import com.microservice.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, UUID> {
+
 }
