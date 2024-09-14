e1df67d4dd76569c4f412cc87d06d92396c6bf74
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-09-14 08:26:30 +0200

    refactored dtos to records

--- inventory/src/main/java/com/microservice/inventory/model/InventoryCreateDTO.java
+++ inventory/src/main/java/com/microservice/inventory/model/InventoryCreateDTO.java
@@ -1,28 +1,6 @@
 package com.microservice.inventory.model;
 
-public class InventoryCreateDTO {
-
-    private final String productName;
-    private final int quantity;
-    private final int reservedQuantity;
-
-    public InventoryCreateDTO(String productName, int quantity, int reservedQuantity) {
-        this.productName = productName;
-        this.quantity = quantity;
-        this.reservedQuantity = reservedQuantity;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public int getReservedQuantity() {
-        return reservedQuantity;
-    }
+public record InventoryCreateDTO(String productName, int quantity, int reservedQuantity) {
 
     public Inventory toInventory() {
         return new Inventory(productName, quantity, reservedQuantity);
--- inventory/src/main/java/com/microservice/inventory/model/InventoryResponseDTO.java
+++ inventory/src/main/java/com/microservice/inventory/model/InventoryResponseDTO.java
@@ -2,35 +2,7 @@ package com.microservice.inventory.model;
 
 import java.util.UUID;
 
-public class InventoryResponseDTO {
-
-    private final UUID id;
-    private final String productName;
-    private final int quantity;
-    private final int reservedQuantity;
-
-    public InventoryResponseDTO(UUID id, String productName, int quantity, int reservedQuantity) {
-        this.id = id;
-        this.productName = productName;
-        this.quantity = quantity;
-        this.reservedQuantity = reservedQuantity;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public int getReservedQuantity() {
-        return reservedQuantity;
-    }
+public record InventoryResponseDTO(UUID id, String productName, int quantity, int reservedQuantity) {
 
     public static InventoryResponseDTO fromInventory(Inventory inventory) {
         return new InventoryResponseDTO(inventory.getId(), inventory.getProductName(), inventory.getQuantity(), inventory.getReservedQuantity());
